798579d9a11ad174e620c9be2f1bbad176a28b01
lehalmeidafc0 <144854855+lehalmeidafc0@users.noreply.github.com>
2024-04-14 11:28:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-let chave = "cebcd482eda57fa9a6714c1c2ba91885"
+document.querySelector(".busca").addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-function colocarNaTela(dados){
-    console.log(dados)
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
-    document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    let input = document.querySelector("#searchInput").value;
+
+    if (input !== "") {
+        clearInfo();
+        showWarning("Carregando...");
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+            input
+        )}&appid=aa040dae81907f6de6682f551f2f9411&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if (json.cod === 200) {
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                tempo: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+        } else {
+            clearInfo();
+        }
+    }
+
+});
+
+function showInfo(json) {
+    showWarning("");
+    document.querySelector(".titulo").innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector(".tempInfo").innerHTML = `${json.tempo} <sup>ºC</sup>`;
+    document.querySelector(".ventoInfo").innerHTML = `${json.windSpeed} <span>Km/h</span>`;
+
+    document.querySelector(".temp img").setAttribute("src", `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+
+    document.querySelector(".ventoPonto").style.transform = `rotate(${json.windAngle - 90}deg)`;
+    document.querySelector(".resultado").style.display = "block";
+
 }
 
-async function buscarCidade(cidade){
-    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
-    cidade + 
-    "&appid=" + 
-    chave + 
-    "&lang=pt_br" +
-    "&units=metric"
-    )
-    .then(resposta => resposta.json())
-
-    colocarNaTela(dados)
+function clearInfo() {
+    showWarning("");
+    document.querySelector(".resultado").style.display = "none";
 }
 
-
-function cliqueiNoBotao(){
-   let cidade = document.querySelector(".input-cidade").value
-
-   buscarCidade(cidade)
-}
+const showWarning = (msg) => {
+    document.querySelector(".aviso").innerHTML = msg;
+};
